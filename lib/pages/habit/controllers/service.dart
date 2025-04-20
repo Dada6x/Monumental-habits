40ddf9d10c47584af9d263ec7df6ad041be08ddb
Dada6x <dada.66.6x.outlook@gmail.com>
2025-04-20 15:27:54 +0300

    could brek coz took Delete out of Service

--- lib/pages/habit/controllers/service.dart
+++ lib/pages/habit/controllers/service.dart
@@ -8,51 +8,5 @@ import 'package:monumental_habits/pages/habit/dashboard_homepage/habitTable.dart
 import 'package:monumental_habits/util/helper.dart';
 
 class HabitService {
-  void deleteHabit(int id) async {
-    try {
-      String apiUrl = 'http://10.0.2.2:8000/api/habits/$id';
-      var response = await dio.delete(
-        apiUrl,
-        options: Options(
-          headers: {
-            'Authorization': 'Bearer ${token!.getString("token")}',
-            'Accept': 'application/json',
-          },
-        ),
-      );
-      if (response.statusCode == 200) {
-        Get.snackbar('Success', 'Habit deleted successfully! ',
-            snackPosition: SnackPosition.BOTTOM,
-            backgroundColor: Colors.white,
-            colorText: const Color(darkOrange),
-            icon: const Icon(Icons.delete));
-        Get.off(() => HomePage());
-        habitTableKey.currentState?.refreshTable();
-      } else {
-        Get.snackbar(
-          'Error',
-          'Failed to delete habit. Try again later. ❌',
-          snackPosition: SnackPosition.BOTTOM,
-          backgroundColor: Colors.orange,
-          colorText: Colors.white,
-          icon: const Icon(
-            Icons.error,
-            color: Colors.red,
-          ),
-        );
-      }
-    } catch (e) {
-      Get.snackbar(
-        'Error',
-        'Error: Unable to delete habit. Please check your connection. ❌',
-        snackPosition: SnackPosition.BOTTOM,
-        backgroundColor: Colors.orange,
-        colorText: Colors.white,
-        icon: const Icon(
-          Icons.error,
-          color: Colors.red,
-        ),
-      );
-    }
-  }
+
 }
--- lib/pages/habit/info/habit_Info.dart
+++ lib/pages/habit/info/habit_Info.dart
@@ -3,9 +3,11 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:monumental_habits/home/homePage.dart';
 import 'package:monumental_habits/main.dart';
 import 'package:monumental_habits/pages/habit/controllers/habitcontroller.dart';
-import 'package:monumental_habits/pages/habit/controllers/service.dart';
+
+import 'package:monumental_habits/pages/habit/dashboard_homepage/habitTable.dart';
 import 'package:monumental_habits/pages/habit/info/calander.dart';
 import 'package:monumental_habits/util/helper.dart';
 import 'package:monumental_habits/util/sizedconfig.dart';
@@ -34,6 +36,54 @@ class _HabitInfoPageState extends State<HabitInfoPage> {
   void initState() {
     super.initState();
     fetchHabitData();
+  }
+
+  void deleteHabit(int id) async {
+    try {
+      String apiUrl = 'http://10.0.2.2:8000/api/habits/$id';
+      var response = await dio.delete(
+        apiUrl,
+        options: Options(
+          headers: {
+            'Authorization': 'Bearer ${token!.getString("token")}',
+            'Accept': 'application/json',
+          },
+        ),
+      );
+      if (response.statusCode == 200) {
+        Get.snackbar('Success', 'Habit deleted successfully! ',
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Colors.white,
+            colorText: const Color(darkOrange),
+            icon: const Icon(Icons.delete));
+        Get.off(() => HomePage());
+        habitTableKey.currentState?.refreshTable();
+      } else {
+        Get.snackbar(
+          'Error',
+          'Failed to delete habit. Try again later. ❌',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.orange,
+          colorText: Colors.white,
+          icon: const Icon(
+            Icons.error,
+            color: Colors.red,
+          ),
+        );
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error',
+        'Error: Unable to delete habit. Please check your connection. ❌',
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.orange,
+        colorText: Colors.white,
+        icon: const Icon(
+          Icons.error,
+          color: Colors.red,
+        ),
+      );
+    }
   }
 
   Future<String> fetchHabitData() async {
@@ -158,7 +208,7 @@ class _HabitInfoPageState extends State<HabitInfoPage> {
                                             .colorScheme
                                             .primary,
                                       ),
-                                      //TODO wait for abdo to fix this 
+                                      //TODO wait for abdo to fix this
                                       const Text("Repeat : days "),
                                     ],
                                   ),
@@ -253,7 +303,7 @@ class _HabitInfoPageState extends State<HabitInfoPage> {
                               ),
                             );
                             if (confirmDelete == true) {
-                              HabitService().deleteHabit(widget.id);
+                              deleteHabit(widget.id);
                             } else {
                               Get.snackbar(
                                 'Cancelled',
@@ -286,6 +336,7 @@ class _HabitInfoPageState extends State<HabitInfoPage> {
       children: children,
     );
   }
+
 //TODO make it look nicer :D @Ward-ikhtiyar plz @
   Widget customWidget(String bigText, String smallText, String assets) {
     return Row(
